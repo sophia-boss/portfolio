0ec80cdf48fff0b95d3a1c6e5b527a94ba34b822
sophiaboss11 <51395437+sophiaboss11@users.noreply.github.com>
2021-05-09 01:45:38 -0700

    reload 360

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ function onVrViewLoad() {
   // Selector '#vrview' finds element with id 'vrview'.
   var vrView = new VRView.Player('#vrview', {
     image: 'assets/360_test2.jpg',
-    is_stereo: false
+    preview:'https://raw.githubusercontent.com/sophia-boss/portfolio/main/assets/360_test2.jpg',
+    is_stereo: false,
+    width: 1200,
+    height: 400
+
   });
 }
